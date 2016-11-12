572b3f5b78342d3c3dae194a6f8e76508fc66895
afr114 <afr114@gmail.com>
2016-11-12 15:26:20 -0500

    add form, js and css

--- airportrun/app/assets/javascripts/application.js
+++ airportrun/app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function openForm() {
+  document.getElementById('sign-up-form').style.display = "block"
+}
+
+document.addEventListener('click', openForm);
