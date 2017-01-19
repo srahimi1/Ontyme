e659088fdd1e185ece86faa7206c09df1d562eec
srahimi1 <salr3467@gmail.com>
2017-01-19 10:10:33 -0500

    added strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,16 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		render plain: params[:user].inspect
+		#render plain: params[:user].inspect
+		
+		@user = User.new(params.require(:user).permit(:nickname, :hotel, :icon))
+
+		if @user.save
+			render plain: "OK"
+		else 
+			render plain: "FAIL"
+		end
+
 	end
 
 end
