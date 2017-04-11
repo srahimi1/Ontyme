fd5f4a72cb7a618ce147057f9260066e43d09e55
unknown <salr3467@gmail.com>
2017-04-11 17:11:32 -0400

    car anim not working 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -122,7 +122,7 @@ function initCarAnim() {
 function carAnim() {
   clearInterval(carAnimID);
   for (i = 0; i < car.length; i++) {
-     console.log(car[i].strokeDasharray+" "+car[i].strokeDashoffset);
+     console.log(car[i].style.strokeDasharray+" "+car[i].style.strokeDashoffset);
     car[i].style.strokeDashoffset = car[i].getTotalLength()-car[i].carCurrentOffset;
     car[i].carCurrentOffset = car[i].carCurrentOffset+car[i].carCurrentOffset*.91;
     if (car[i].carCurrentOffset > car[i].getTotalLength()) {
