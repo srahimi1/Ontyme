37aa4412a090b87a857b03c2d1d71ec73bfbef63
unknown <salr3467@gmail.com>
2017-04-10 21:32:30 -0400

    fix container sizes 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -67,10 +67,12 @@ function positionSVGS() {
   if (screenWidth < 401) {
     logoPhrase2.setAttribute("font-size","15");
     logoPhrase2.setAttribute("dy", "18");
+    logoPhrase1.style="display:none";
   }
   else if (screenWidth < 601) {
     logoPhrase2.setAttribute("font-size","20");
     logoPhrase2.setAttribute("dy","24");
+    logoPhrase1.style="display:none";
   }
  centerSVG(logoPhrase2, midScreen, 0, 1, 1); 
  var rideComfortably = document.getElementById("rideComfortably").childNodes[1].childNodes[1];
@@ -96,11 +98,11 @@ function centerSVG(element, midScreen, shiftY, scaleX, scaleY) {
     var shiftNew = midScreen-(left+mid);
     shiftX += shiftNew;
     shiftY = top - element.parentNode.getBoundingClientRect().top;
-    shiftY -= 5;
+    shiftY += 5;
     shiftY = 0 - shiftY; 
     element.setAttribute("transform","scale("+scaleX+" "+scaleY+") translate("+shiftX+" "+shiftY+")");
     element.parentNode.setAttribute("width", window.innerWidth);
-    var ySize = element.parentNode.getBoundingClientRect().height + 10;
+    var ySize = element.getBoundingClientRect().height + 10;
     element.parentNode.setAttribute("height", ySize);
     return 1;
 }
