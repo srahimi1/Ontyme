a8df895ce2257deca265e270a37a79b7d9ffe798
none <salr3467@gmail.com>
2017-05-11 10:20:00 -0400

    add find address response from third party

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+	get '/users/getaddress', to: 'users#findAddress'
+
 	resources :users
 	resources :trips
 	resources :hotels
 
   	root 'welcome#index'
 
- 	post "/users/getaddress", to: "users#findAddress"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
