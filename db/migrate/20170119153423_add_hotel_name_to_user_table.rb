8ec0e45edfe8f04651486b9e45f662eafba7d07e
none <salr3467@gmail.com>
2017-05-16 18:59:05 -0400

    deleted old migrations and db/schema.rb

--- db/migrate/20170119153423_add_hotel_name_to_user_table.rb
+++
@@ -1,5 +0,0 @@
-class AddHotelNameToUserTable < ActiveRecord::Migration[5.0]
-  def up
-  	add_column :users, :hotel_name, :string
-  end
-end
--- db/migrate/20170227164221_change_trips_table_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTripsTableName < ActiveRecord::Migration[5.0]
-  def change
-  	rename_table :trips, :active_trips
-  end
-end
