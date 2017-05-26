2417e0131a7127361bf1ad080fab9bd5939f20f7
none <salr3467@gmail.com>
2017-05-26 13:16:14 -0400

    create trip_request model method to generate custom trip_request_id

--- db/migrate/20170516221036_create_driver_current_statuses.rb
+++ db/migrate/20170516221036_create_driver_current_statuses.rb
@@ -3,6 +3,8 @@ class CreateDriverCurrentStatuses < ActiveRecord::Migration[5.0]
     create_table :driver_current_statuses do |t|
     	t.references :driver, foreign_key: true
     	t.string :status
+    	t.string :current_longitude
+    	t.string :current_latitude
 		t.timestamps
     end
   end
