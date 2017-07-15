f26315e93133afc8fc2bb2056ef9ef395bdac1ac
none <salr3467@gmail.com>
2017-07-15 13:27:29 -0400

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	self.primary_key = "user_id2"
 	has_many :trip_requests, :foreign_key => "trip_request_id2"
 	has_many :competed_trips, :foreign_key => "completed_trip_id2"
 	has_one :active_trip, :foreign_key => "active_trip_id2"
