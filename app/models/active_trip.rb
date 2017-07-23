f8dbb55665dcfd6d250d6317be81cba33867aae2
none <salr3467@gmail.com>
2017-07-23 15:26:05 -0400

    add attre_accessor to active trip model to allow for assign attributes method to work

--- app/models/active_trip.rb
+++ app/models/active_trip.rb
@@ -3,4 +3,6 @@ class ActiveTrip < ApplicationRecord
 	belongs_to :user, :foreign_key => "user_id2"
 	belongs_to :driver, :foreign_key => "driver_id2"
 	belongs_to :trip_request, :foreign_key => "trip_request_id2"
+
+	attr_accessor :active_trip_id2, :user_id2, :driver_id2, :trip_request_id2, :map_provider, :map_provider_url, :destination_street, :destination_city, :destination_state, :destination_postalcode, :destination_longitude, :destination_latitude, :map_provider_destination_id, :map_provider_destination_slug, :driver_connect_time, :trip_start_time, :trip_end_time, :response_from_routing_service_seg_1, :response_from_routing_service_seg_2, :route_data, :status
 end
