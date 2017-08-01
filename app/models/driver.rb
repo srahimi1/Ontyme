abd5043e5eb3a91d0b0f99e7ec05d064e0cb7095
none <salr3467@gmail.com>
2017-08-01 18:04:17 -0400

    a

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -12,7 +12,7 @@ class Driver < ApplicationRecord
 		uri = URI.parse(url)
 		connection = Net::HTTP.new(uri.host, uri.port)
 		res = connection.get(uri.request_uri)
-		if active_trip_id != nil
+		if !!active_trip_id
 			a = ActiveTrip.find_by(active_trip_id2: active_trip_id)
 			a.update_attributes(:response_from_routing_service_seg_1 => res.body)
 		end
