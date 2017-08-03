58f24fcba1ccf31ff16de76ecb73e36dd7d78e2b
none <salr3467@gmail.com>
2017-08-02 21:36:51 -0400

    a

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -110,7 +110,7 @@ class DriversController < ApplicationController
 	end
 
 	def getDirections
-		trip_request = ActiveTrip.find_by(trip_request_id2: params[:trip_request_id])
+		trip_request = TripRequest.find_by(trip_request_id2: params[:trip_request_id])
 		directions = Driver.get_directions(nil, params[:longitude].to_s, params[:latitude].to_s, trip_request.pickup_longitude, trip_request.pickup_latitude)
 		render plain: directions
 	end
