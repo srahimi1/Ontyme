1c39a13d6fb52647211ef9a12cf99d7ce2e9dbf0
none <salr3467@gmail.com>
2017-08-04 16:28:59 -0400

    a

--- public/javascripts/checkForRideRequests.js
+++ public/javascripts/checkForRideRequests.js
@@ -9,10 +9,12 @@ var aa;
 onmessage = function(event2) {
 	var coordinates2 = event2.data[0]; 
 	aa = event2.data;
-	console.log(event2.data[0]);
+	console.log(coordinates2);
 	
 	status = event2.data[1];
 	if (!status) {
+		console.log(coordinates2);
+		console.log(coordinates2.longitude);
 		url = "/drivers/checkForRideRequests?longitude="+coordinates2.longitude+"&latitude="+coordinates2.latitude;
 		startEventStream();
 	}
