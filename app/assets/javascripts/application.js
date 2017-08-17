a917aa345907b3f83bcc53fe23cd5479768ad2e1
none <salr3467@gmail.com>
2017-08-17 13:53:03 -0400

    a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -310,7 +310,6 @@ function showOnMap(extentTemp, directionsTemp, geometryTemp, colorTemp) {
   
   if (router.status == 2) {
     map.once("postcompose", function(event){
-      alert("in map postcompose");
       Nav(); // setTimeout(function () { map.getView().animate({ zoom: zoomA }) }, 200);
     });
   } // end if (router.status == 2)
@@ -338,7 +337,6 @@ function showOnMap(extentTemp, directionsTemp, geometryTemp, colorTemp) {
     }) );
     
     router.vectorSource.addFeature(feature);
-      alert("in new steps add to vector for loop ");
     } // end for (var i = 0; i < router.steps.length; i++)
 
     map.getLayers().item(1).setSource( router.vectorSource );
