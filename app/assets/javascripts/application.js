967719a85eaaaf0015162f63862e7849aaf53221
none <salr3467@gmail.com>
2018-01-09 14:40:20 -0500

    create setFeatureSize function 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -264,13 +264,13 @@ function testnav() {
 
 
 function setFeatureSize( tempMap, lineColor ) {
-  if ( tempMap.getView().getZoom() ) {
     var wid = Math.ceil( (4.14 * tempMap.getView().getZoom() )  - 50 );
+  if (wid > 0) {
     tempMap.getLayers().item(1).getSource().getFeatures()[0].setStyle( new ol.style.Style({
         stroke: new ol.style.Stroke({ width: wid, color: lineColor })
       })  );
     console.log(" in setFeatureSize and wid is " + wid);
-  } // end if ( tempMap.getView().getZoom() ) 
+  } // end if ( wid > 0 ) 
 } // end function setFeatureSize( tempMap, lineColor )
 
 function showOnMap(extentTemp, directionsTemp, geometryTemp, colorTemp) {
