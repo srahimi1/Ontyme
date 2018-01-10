a6798b50ee7dd1c9d3026d7aa6923717665d1530
none <salr3467@gmail.com>
2018-01-10 10:35:06 -0500

    rerouting debugging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -120,7 +120,7 @@ function ifTurnedAtIntersection( instance ) {
     console.log("inters - " + inters);
     for (var j = 0; j < inters.length; j++) {
         var tempDist = getGeodesicDistance(coordinates2,inters[j].location);
-        instruction.innerHTML = "" + tempDist + "";
+        instruction.innerHTML = "" + tempDist + "" + i + " " + j;
         if (tempDist < 10) {instance.currentIntersectionIndex = j; alert("at intersection"); atIntersection = 1; break;}
     } 
     if (atIntersection) {instance.currentIntersection = inters[instance.currentIntersectionIndex]; break;}
